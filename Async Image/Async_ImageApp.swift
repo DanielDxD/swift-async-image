1e7a58e705ddaf2f8b27cb71afdc87c65f5858a9
Daniel Lopes <daniellopes@Daniels-iMac-Pro.local>
2022-05-16 17:36:36 -0300

    First commit

---
+++ Async Image/Async_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_ImageApp.swift
+//  Async Image
+//
+//  Created by Daniel Lopes on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Async_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Async Image/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Async Image
+//
+//  Created by Daniel Lopes on 16/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
+    var body: some View {
+        // MARK: - 1. BASIC
+        // AsyncImage(url: URL(string: imageURL))
+        // MARK: - 2. SCALE
+        // AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        // MARK: 3 - PLACEHOLDER
+        AsyncImage(url: URL(string: imageURL)) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 128)
+                .foregroundColor(.purple)
+                .opacity(0.5)
+        }
+        .padding(40)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
