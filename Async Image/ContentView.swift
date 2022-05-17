98b20bcdb1952c3d83759fa3fc69643e066875a2
Daniel Lopes <daniellopes@Daniels-iMac-Pro.local>
2022-05-16 23:03:55 -0300

    Test others animations

--- Async Image/ContentView.swift
+++ Async Image/ContentView.swift
@@ -58,7 +58,11 @@ struct ContentView: View {
         AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
             switch phase {
             case .success(let image):
-                image.imageModifier()
+                image
+                    .imageModifier()
+                    // .transition(.move(edge: .bottom))
+                    //.transition(.slide)
+                    .transition(.scale)
             case .failure(_):
                 Image(systemName: "ant.circle.fill")
                     .iconModifier()
